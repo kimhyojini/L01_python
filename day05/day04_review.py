a94fe39c1042378670013d8490e9bb6d33af1dc2
educhel <node.chelsea@gmail.com>
2025-12-29 09:14:54 +0900

    [Day05] 복습 실습

---
+++ day05/day04_review.py
@@ -0,0 +1,18 @@
+# 복습
+# 다양한 자료를 이용하여, 정보를 구조화하는 방식이 알고리즘에서 중요하다.
+
+# 복습 실습 
+users = {
+        'total_user': 3,
+        'information': [
+            {'name': 'alex', 'age':3, 'license':True},
+            {'name': 'june', 'age':7, 'license':False},
+            {'name': 'peter', 'age':4, 'license':False}
+        ]
+        }
+
+# Q1. 라이센스가 있는 인원들의 숫자 구하기
+
+# Q2. 모든 사람의 나이 평균 구하기
+
+# Q3. 라이센스가 없는 사람들의 이름 모으기
